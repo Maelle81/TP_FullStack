c1331c726d03df3d9b3ecfb654d12b21e61f9553
MaelleRampnoux <79195580+Maelle81@users.noreply.github.com>
2022-03-27 12:01:19 +0200

    Add files via upload

---
+++ src/components/CityCountryList.vue
@@ -0,0 +1,47 @@
+<template>
+    
+    <!--<p>coucou</p>
+    <div class="form-group">
+        <label v-for="country in data.countries" :key="country.id">Pays :</label>
+        <select class ="form-control" @click="$emit('eventCity', country.name)">
+            <option disabled="" value="">Choisissez un pays</option>
+            <option v-model="name">{{country.name}}</option> 
+        </select>       
+    </div>
+    <div id="cities">
+        <table>
+            <tr><th>Nom</th><th>Population (en millions)</th></tr>
+            <tr><td>{{city.nom}}</td><td>{{city.population}}</td></tr>
+        </table>
+    </div>  -->
+    
+    <div>
+    <table class="table table-bordered table-sm table-hover">
+      <thead>
+        <tr>
+          <th>Nom</th>
+          <th>Population</th>          
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="city in cities" :key="city.id">
+          <td>{{ city.name }}</td>
+          <td>{{ city.population }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+   
+</template>
+
+<script setup>
+    const props = defineProps({
+  cities: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+
---
+++ src/views/CitiesByCountry.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="">
+    <h2>Les villes par pays</h2>
+    <div>
+      <div class="col">
+        <label id="countries" for="countries">Pays:</label>
+            <select id="country-selector" @input="fetchCities">
+              <option selected disabled hidden>Choisissez un pays</option>
+              <option
+                v-for="country in data.allCountries"
+                :key="country.id"
+                :value="country._links.cities.href"
+              >
+                {{ country.name }}
+              </option>
+            </select>                 
+      </div>
+      <div>
+        <CityCountryList v-bind:cities="data.allCities" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive, onMounted } from "vue";
+// @ is an alias to /src
+import CityCountryList from "@/components/CityCountryList.vue";
+
+        const data = reactive({
+          allCountries: [],
+          allCities: [],          
+        });
+
+        function refresh() {
+          fetch("api/countries")
+              .then((response) => {
+                if (!response.ok) { // status != 2XX
+                  throw new Error(response.status);
+                }
+                 return response.json();
+              })
+              .then((json) => {
+                data.allCountries = json._embedded.countries;
+              })
+              .catch((error) => alert(error));
+        }
+
+        function fetchCities() {
+            fetch(document.getElementById("country-selector").value)
+            .then((response) => response.json())
+            .then((json) => {
+              data.allCities =[];
+              let listCity = json._embedded.cities;
+              for(let i=0; i<listCity.length;i++){
+                data.allCities.push(listCity[i])
+              }
+            })
+            .catch((error) => alert(error));
+        }      
+        
+        onMounted(() => {
+          refresh();
+          fetchCities(); // On récupère les villes (pour la table)
+          
+        });
+
+</script>
+
